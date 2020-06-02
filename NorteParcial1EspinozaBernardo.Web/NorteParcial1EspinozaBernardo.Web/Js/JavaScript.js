36ee57ad2cd4b7e03b0470627ca8a2e4ec6713a5
BERNARDO ESPINOZA <b-j-espinoza@hotmail.com>
2020-06-01 21:44:38 -0300

    JS

---
+++ NorteParcial1EspinozaBernardo.Web/NorteParcial1EspinozaBernardo.Web/Js/JavaScript.js
@@ -0,0 +1,37 @@
+﻿
+
+    var constelacion = new Array();
+
+        function Constelacion() {
+
+
+            this.setNombre = function (value) { this._nombre = value; }
+            this.getNombre = function () { return this._nombre; }
+        this.setsiglo = function (value) { this._codigo = value; }
+            this.getsiglo = function () { return this._codigo; }
+
+            
+        }
+
+
+        $("#btnSave").click(function () {
+
+            var pro = new Producto();
+            pro.setCodigo($("#txtSiglo").val());
+            pro.setNombre($("#txtNombre").val());
+
+            productos.push(pro);
+            $("#txtSiglo").val("");
+            $("#txtNombre").val("");
+        });
+
+        $("#btnList").click(function () {
+
+            var ctl = $("#resultado");
+            ctl.html("");
+            $.each(productos, function (i, e) {
+        ctl.append("Siglo: " + e.getCodigo() + " - " + "Nombre: " + e.getNombre() + "<br />");
+            });
+        });
+
+   
